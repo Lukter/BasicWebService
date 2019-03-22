c52217b41a6b0360f49026795dbd5c25f572d595
Lucas Lucindo Vieira <llucindov@gmail.com>
2019-03-21 21:26:55 -0300

    Add basic validation functions
    Two validation functions were implemented. One validates whether the path can be transformed into an integer and the other validates if that integer is in the range -99999 and 99999.

--- source/server.py
+++ source/server.py
@@ -5,10 +5,42 @@ from flask import Flask,jsonify
 app = Flask(__name__)
 
 
+def validatePath(path):
+    try:
+        path_as_integer = int(path)
+        print('Path is ok!')
+        return True
+    except ValueError as e:
+        print('Invalid path! Cannot resolve...')
+        return False
+
+def validateRange(path):
+    path_as_integer = int(path)
+    if -99999 <= path_as_integer <= 99999:
+        print('Number in a valid range!')
+        return True
+    else:
+        print('Invalid number request! Cannot resolve...')
+        return False
+
+def validateRequest(path):
+    if not validatePath(path):
+        return False
+
+    if not validateRange(path):
+        return False
+
+    return True
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
-    return jsonify({"message":"Hello Json!"})
+    
+    if validateRequest(path):
+        path = int(path)
+        return jsonify({"extenso": path})
+    else:
+        return jsonify({"ERRO": path})
 
 if __name__ == '__main__':
     app.run()
