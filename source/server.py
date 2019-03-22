a3bd92695f3c7ffda2c24cb328453e85a72cb3c2
Lucas Lucindo Vieira <llucindov@gmail.com>
2019-03-22 00:01:20 -0300

    Add function to convert request url number to in full form.
    int2word implements a routine to convert numbers from range 0 - 99999 to in full form. This conversions is done in Portuguese.

--- source/server.py
+++ source/server.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python
 
+import int2word
 from flask import Flask,jsonify
 
 app = Flask(__name__)
 
+UNIT = {'00': '', '01': 'um', '02': 'dois', '03': 'tres', '04': 'quatro', '05': 'cinco',
+        '06': 'seis', '07': 'sete', '08': 'oito', '09': 'nove', '10': 'dez', '11': 'onze',
+        '12': 'doze', '13': 'treze', '14': 'quatorze', '15': 'quinze', '16': 'dezesseis',
+        '17': 'dezessete', '18': 'dezoito', '19': 'dezenove'}
+
+TENS = {'0': '', '2': 'vinte', '3': 'trinta', '4': 'quarenta', '5': 'cinquenta', '6': 'sessenta',
+        '7': 'setenta', '8': 'oitenta', '9': 'noventa'}
+
+HUNDRED = {'0': '', '1': 'cento', '2': 'duzentos', '3': 'trezentos', '4': 'quatrocentos', '5': 'quinhentos',
+           '6': 'seiscentos', '7': 'setecentos', '8': 'oitocentos', '9': 'novecentos'}
+
+THOUSAND = {'00': '', '01': 'mil'}
 
 def validatePath(path):
     try:
@@ -35,12 +48,11 @@ def validateRequest(path):
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
-    
+
     if validateRequest(path):
-        path = int(path)
-        return jsonify({"extenso": path})
+        return jsonify(int2word.writter(path))
     else:
-        return jsonify({"ERRO": path})
+        return jsonify({"ERRO": 'Cannot resolve'})
 
 if __name__ == '__main__':
     app.run()
--- tests/validate_paths.py
+++ tests/validate_paths.py
@@ -2,10 +2,10 @@ import requests
 import json
 
 response = requests.get('http://localhost:5000/1')
-assert(response.json() == {'extenso': 1})
+assert(response.json() == {'extenso': '1'})
 
 response = requests.get('http://localhost:5000/0')
-assert(response.json() == {'extenso': 0})
+assert(response.json() == {'extenso': '0'})
 
 response = requests.get('http://localhost:5000/1.1')
 assert(response.json() == {'ERRO': '1.1'})
@@ -14,13 +14,13 @@ response = requests.get('http://localhost:5000/1,1')
 assert(response.json() == {'ERRO': '1,1'})
 
 response = requests.get('http://localhost:5000/99998')
-assert(response.json() == {'extenso': 99998})
+assert(response.json() == {'extenso': '99998'})
 
 response = requests.get('http://localhost:5000/99999')
-assert(response.json() == {'extenso': 99999})
+assert(response.json() == {'extenso': '99999'})
 
 response = requests.get('http://localhost:5000/-1')
-assert(response.json() == {'extenso': -1})
+assert(response.json() == {'extenso': '-1'})
 
 response = requests.get('http://localhost:5000/-1.1')
 assert(response.json() == {'ERRO': '-1.1'})
@@ -29,10 +29,10 @@ response = requests.get('http://localhost:5000/-1,1')
 assert(response.json() == {'ERRO': '-1,1'})
 
 response = requests.get('http://localhost:5000/-99998')
-assert(response.json() == {'extenso': -99998})
+assert(response.json() == {'extenso': '-99998'})
 
 response = requests.get('http://localhost:5000/-99999')
-assert(response.json() == {'extenso': -99999})
+assert(response.json() == {'extenso': '-99999'})
 
 response = requests.get('http://localhost:5000/9999999999999')
 assert(response.json() == {'ERRO': '9999999999999'})
