aba2746cd42b0e3e7632a9df3efc58dd4add41cb
Lucas Lucindo Vieira <llucindov@gmail.com>
2019-03-26 18:15:35 -0300

    Remove repetead/useless code

--- source/server.py
+++ source/server.py
@@ -5,22 +5,6 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-UNIT = {'00': '', '01': 'um', '02': 'dois', '03': 'tres', '04': 'quatro',
-        '05': 'cinco', '06': 'seis', '07': 'sete', '08': 'oito',
-        '09': 'nove', '10': 'dez', '11': 'onze', '12': 'doze', '13': 'treze',
-        '14': 'quatorze', '15': 'quinze', '16': 'dezesseis', '17': 'dezessete',
-        '18': 'dezoito', '19': 'dezenove'}
-
-TENS = {'0': '', '2': 'vinte', '3': 'trinta', '4': 'quarenta',
-        '5': 'cinquenta', '6': 'sessenta', '7': 'setenta',
-        '8': 'oitenta', '9': 'noventa'}
-
-HUNDRED = {'0': '', '1': 'cento', '2': 'duzentos', '3': 'trezentos',
-           '4': 'quatrocentos', '5': 'quinhentos', '6': 'seiscentos',
-           '7': 'setecentos', '8': 'oitocentos', '9': 'novecentos'}
-
-THOUSAND = {'00': '', '01': 'mil'}
-
 
 def validatePath(path):
     try:
@@ -43,14 +27,8 @@ def validateRange(path):
 
 
 def validateRequest(path):
-    if not validatePath(path):
-        return False
 
-    if not validateRange(path):
-        return False
-
-    return True
-
+    return validatePath(path) and validateRange(path)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
